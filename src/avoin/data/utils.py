5aea663dd4d21c9a574907c47d8d046c7c2cc512
Ville Korhonen <ville@xd.fi>
2012-10-12 07:08:03 +0300

    Initial data package, stubs for converter functions

---
+++ src/avoin/data/utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import json
+import csv
+
+def json_to_csv(content):
+	raise NotImplementedError
+
+def csv_to_json(content):
+	raise NotImplementedError
+
+def json_to_sql(content):
+	raise NotImplementedError
