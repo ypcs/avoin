450be744694ff83d8bfaaccb7b859c0802f42ecd
Ville Korhonen <ville@xd.fi>
2012-10-12 07:25:54 +0300

    More functions stubs, added test to add_unique_to_list

--- src/avoin/data/utils.py
+++ src/avoin/data/utils.py
@@ -3,6 +3,54 @@
 import json
 import csv
 
+def add_unique_to_list(item, uniq_list):
+	'''Add only unique items to list
+	>>> from utils import add_unique_to_list
+	>>> test_list = []
+	>>> add_unique_to_list('test1', test_list)
+	True
+	>>> add_unique_to_list('test1', test_list)
+	False
+	>>> add_unique_to_list('test2', test_list)
+	True
+	>>> add_unique_to_list('test2', test_list)
+	False
+	>>> add_unique_to_list('test1', test_list)
+	False
+	>>> add_unique_to_list('test3', test_list)
+	True
+	'''
+	if not item in uniq_list:
+		uniq_list.append(item)
+		return True
+	else:
+		return False
+
+def _dict_to_csv(items):
+	raise NotImplementedError
+	
+	# TODO: Handle 3D lists etc.
+
+	headers = []
+
+	# Expand headers w/ dict keys
+	for i in items:
+		add_unique_to_list(i, headers)
+
+
+
+def _dict_to_json(items):
+	return json.dumps(items)
+
+def _dict_to_sql(items):
+	pass
+
+def _json_to_dict(content):
+	return json.loads(content)
+
+def _csv_to_dict(content):
+	raise NotImplementedError
+
 def json_to_csv(content):
 	raise NotImplementedError
 
