4d6d329d6a5c5c1ca5d9238e6ff8c43dd2bd2f98
Ville Korhonen <ville@xd.fi>
2015-08-14 23:27:33 +0300

    Add initial database handling

--- src/avoin/data/db.py
+++ src/avoin/data/db.py
@@ -2,3 +2,22 @@
 
 from pymongo import MongoClient
 
+class Database(object):
+    def __init__(self, database, **kwargs):
+        self._client = MongoClient()
+        self._database = self._client[database]
+        self._kwargs = kwargs
+
+    def collection(self, name):
+        return self._database[name]
+
+def main():
+    db = Database('test')
+    restaurants = db.collection('restaurants')
+    print restaurants.find_one()
+    
+    return 0
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
