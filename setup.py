c988a06019f39dd50739b5bd0b52409063d5b857
Ville Korhonen <ville@xd.fi>
2019-03-19 16:27:04 +0200

    WIP use requirements.txt

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__package_name = 'avoin'
-
 import os
+
 from setuptools import setup, find_packages
 from version import get_git_version
+
 
 def read(filename):
     f = os.path.join(os.path.dirname(__file__), filename)
     if os.path.exists(f):
         return open(f).read()
 
-setup(name=__package_name,
+
+setup(name='avoin',
     version = get_git_version(),
-    description='Misc python tools',
+    description="Misc python tools",
     long_description=read('README.md'),
     author='Ville Korhonen',
     author_email='ville@xd.fi',
-    url='https://github.com/ypcs/' + __package_name,
+    url='https://github.com/ypcs/avoin',
     packages=find_packages('src'),
     package_dir={'': 'src',},
-    install_requires=[
-        'requests',
-        'requests_cache',
-        'lxml',
-    ],
+    install_requires=open('requirements.txt', 'r').readlines(),
     license='GPLv3',
     classifiers=[
         'Development Status :: 3 - Alpha',
